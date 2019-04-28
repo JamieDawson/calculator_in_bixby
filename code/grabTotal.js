b47730d0894c3c46e7d7b8d2aba22251b6dc5d88
Jamie Dawson <jadawson@e1z3r7p9.42.us.org>
2019-04-28 11:44:58 -0700

    added training. Might add more training to handle different sitation

--- code/grabTotal.js
+++ code/grabTotal.js
@@ -8,8 +8,10 @@ module.exports.function = function grabTotal (firstNumber, secondNumber, operato
     combine = firstNumber - secondNumber;
   else if (operator == "multiply" || operator == "multiplied" || operator == "times")
     combine = firstNumber * secondNumber;
-  else (operator == "divided")
+  else if (operator == "divided")
     combine = firstNumber / secondNumber;
+  else
+    combine = -5;
     
   return combine;
 }
